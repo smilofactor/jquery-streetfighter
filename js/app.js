ee2b191781fc072b0b021b5b5aa37ed0ee9334cd
Steve M <smilovidov@gmail.com>
2015-06-14 23:34:57 -0400

    Javascript ryu animation experiment

--- js/app.js
+++ js/app.js
@@ -3,6 +3,19 @@ $(function() {
 	.mouseover(function() {
 		$(".ryu-still").hide();
 		$(".ryu-ready").show();
+
+		$(".ryu-ready")
+		.mousedown(function() {
+			$(".ryu-ready").hide();
+			$(".ryu-throwing").show();
+
+			$(".ryu-throwing")
+			.mouseup(function() {
+				$(".ryu-throwing").hide();
+				$(".ryu-ready").show();
+
+			});
+		});
 	});
 
     $(".ryu-ready")
