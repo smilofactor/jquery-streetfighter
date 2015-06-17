5e353b48a5a0278c817776300bd215bdddf332f1
Steve M <smilovidov@gmail.com>
2015-06-17 09:31:27 -0400

    Mostly experiment with js/app.js

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
 $(function() {
 	$(".ryu-still")
-	.mouseover(function() {
+	.mouseenter(function() {
 		$(".ryu-still").hide();
 		$(".ryu-ready").show();
+    })
+    .mouseleave(function() {
+    	$(".ryu-ready").hide();
+    	$(".ryu-still").show();
+    })
+    .mousedown(function() {
+        $(".ryu-still").hide();
+        $(".ryu-throwing").show();
 
+    })
+});
+
+
+
+        /*
 		$(".ryu-ready")
 		.mousedown(function() {
 			$(".ryu-still").hide();
@@ -19,11 +33,5 @@ $(function() {
 			});
 		});
 	});
+    */
 
-    $(".ryu-ready")
-    .mouseleave(function() {
-    	$(".ryu-ready").hide();
-    	$(".ryu-still").show();
-    });
-
-});
