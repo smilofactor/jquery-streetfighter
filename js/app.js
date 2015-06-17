641b393c26715d4f0024181f7259760343e0b543
Steve M <smilovidov@gmail.com>
2015-06-17 13:47:51 -0400

    Refining gameview and hadouken in css, refactored main.html with additional view, refactored js/app.js

--- js/app.js
+++ js/app.js
@@ -1,41 +1,30 @@
 $(document).ready(function() {
-	$(".ryu-game")
-	.mouseenter(function() {
-		$(".ryu-still").hide().
-        mouseleave(function() {
-            $(this).show();
-        })
-		$(".ryu-ready").show().
-        mouseleave(function() {
-            $(this).hide();
-        })
-    })
-    .mouseleave(function() {
-    	$(".ryu-ready").hide();
-    	$(".ryu-still").show();
-    })
-    .mousedown(function() {
-        $(".ryu-still").hide()
-        .mouseleave(function() {
-            $(this).show();
-        })
-        $(".ryu-throwing").show()
-        .mouseleave(function() {
-            $(this).hide();
-        })
-        $(".hadouken").finish().show()
-        .animate(
+	$('.ryu-game').mouseenter(function() {
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    }).mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').hide();
+    }).mousedown(function() {
+        $('.ryu-throwing').show();
+        $('.ryu-ready').hide();
+        $('.hadouken').finish().show().animate(
             {'left': '1020px'},
             500,
             function() {
                 $(this).hide();
                 $(this).css('left', '520px');
-            }
-            );
-
-    })
-    .mouseup(function() {
-        $(".ryu-still").show();
-        $(".ryu-throwing").hide();
-    })
+            });
+        })
+        .mouseup(function() {
+            $('.ryu-throwing').hide();
+            $('.ryu-ready').show();
+        });
 });
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
