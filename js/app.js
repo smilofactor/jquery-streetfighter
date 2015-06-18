451ef77ea1c0c2b640fd8097484cf9570055bb23
Steve M <smilovidov@gmail.com>
2015-06-18 15:59:21 -0400

    Brought main.html, main.css, app.js up to date.Extensive refactoring

--- js/app.js
+++ js/app.js
@@ -1,29 +1,41 @@
-$(function() {
-	$(".ryu-still")
-	.mouseover(function() {
-		$(".ryu-still").hide();
-		$(".ryu-ready").show();
+$(document).ready(function() {
+	$('.ryu-game').mouseenter(function() {
+        $('.ryu-image').hide();
+        $('.ryu-ready').show();
+    }).mouseleave(function() {
+        $('.ryu-image').hide();
+        $('.ryu-still').show();
+    }).mousedown(function() {
+        playHadouken();
+        $('.ryu-image').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function() {
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+    }).mouseup(function() {
+            $('.ryu-image').hide();
+            $('.ryu-ready').show();
+    });
+    
+    $(document).keydown(function(ekskey) {
+        if (ekskey.which == '88') {
+            $('.ryu-image').hide();
+            $('.ryu-cool').show();
+            }
+        }).keyup(function() {
+            $('.ryu-image').hide();
+            $('.ryu-still').show();
 
-		$(".ryu-ready")
-		.mousedown(function() {
-			$(".ryu-still").hide();
-			$(".ryu-ready").hide();
-			$(".ryu-throwing").show();
-		});
-	});
-
-			$(".ryu-throwing")
-			.mouseup(function() {
-				$(".ryu-throwing").hide();
-				$(".ryu-ready").hide();
-				$(".ryu-still").show();
-			});
-
-
-    $(".ryu-ready")
-    .mouseleave(function() {
-    	$(".ryu-ready").hide();
-    	$(".ryu-still").show();
+        });
     });
 
-});
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 0.5;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
